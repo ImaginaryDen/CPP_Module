af0459cb1f0a0b18bad45a0375616bc3708e1f9c
ImaginaryDen <denis.marakov@mail.ru>
2022-03-18 15:32:16 +0300

    add 00/ex00

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <locale>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return(1);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (size_t j = 0; argv[i][j]; j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
