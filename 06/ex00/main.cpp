4446dc969e24cddb4540f68c996d5c726f85b9ef
ImaginaryDen <denis.marakov@mail.ru>
2022-04-15 22:10:05 +0300

    add 06/ex00

---
+++ 06/ex00/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stdlib.h>
+#include <climits>
+#include <cerrno>
+#include <iomanip>
+
+double to_double(const std::string &s)
+{
+	char *end;
+	errno = 0;
+	double result = std::strtod(s.c_str(), &end);
+	if (errno == ERANGE)
+		throw std::out_of_range("todouble: string is out of range");
+	if (s.length() == 0 || *end != '\0')
+		throw std::invalid_argument("todouble: invalid string");
+	return result;
+}
+
+void cast_char(std::string str)
+{
+	std::cout << "char: ";
+	if (str.size() == 1 && !isdigit(str[0]))
+	{
+		std::cout << str[0] << std::endl;
+		return ;
+	}
+	try
+	{
+		if (isprint(static_cast<int>(to_double(str))))
+			std::cout << static_cast<char>(to_double(str)) << std::endl;
+		else
+			std::cout <<  "Non displayable" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout <<  "impossible" << std::endl;
+	}
+	
+}
+
+void cast_int(std::string str)
+{
+	try
+	{
+		std::cout << "int: " << static_cast<int>(to_double(str)) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout <<  "impossible" << std::endl;
+	}
+}
+
+void cast_double(std::string str)
+{
+	try
+	{
+		std::cout  << std::fixed << std::setprecision(1) << "double: " << to_double(str) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "non" << std::endl;
+	}
+}
+
+int main(int argc, char **argv)
+{
+
+	if(argc != 2)
+		return (1);
+	cast_char(argv[1]);
+	cast_int(argv[1]);
+	cast_double(argv[1]);
+	return 0;
+}
