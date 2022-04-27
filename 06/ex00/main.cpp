4f210410b5e164260d3df6beb578e9b08bc45243
Tomard Jamis <tjamis@su-b2.msk.21-school.ru>
2022-04-27 15:44:54 +0300

    fix 06/ex00

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -4,6 +4,30 @@
 #include <cerrno>
 #include <iomanip>
 
+void nan_print()
+{
+	std::cout << "char: impossible"<< std::endl;
+	std::cout << "int: impossible"<< std::endl;
+	std::cout << "double: nan"<< std::endl;
+	std::cout << "float: nanf"<< std::endl;
+}
+
+void inf_print(int i)
+{
+	std::cout << "char: impossible"<< std::endl;
+	std::cout << "int: impossible"<< std::endl;
+	if (i == 0)
+	{
+		std::cout << "double: +inf"<< std::endl;
+		std::cout << "float: +inff"<< std::endl;
+	}
+	else
+	{
+		std::cout << "double: -inf"<< std::endl;
+		std::cout << "float: -inff"<< std::endl;
+	}
+}
+
 double to_double(std::string &s)
 {
 	char *end;
@@ -69,17 +93,30 @@ void cast_float(std::string str)
 	}
 	catch(const std::exception& e)
 	{
-		std::cout << "nan" << std::endl;
+		std::cout << "nanf" << std::endl;
 	}
 }
 
 int main(int argc, char **argv)
 {
+	std::string arg = argv[1];
+	int i = std::string("+-").find(arg[0]) != std::string::npos;
+	std::cout << i;
 	if(argc != 2)
 		return (1);
-	cast_char(argv[1]);
-	cast_int(argv[1]);
-	cast_double(argv[1]);
-	cast_float(argv[1]);
+	if(!arg.compare(0, 3, "nan"))
+	{
+		nan_print();
+		return 0;
+	}
+	if(!arg.compare(i, 3, "inf"))
+	{
+		inf_print(arg[0] == '-');
+		return 0;
+	}
+	cast_char(arg);
+	cast_int(arg);
+	cast_double(arg);
+	cast_float(arg);
 	return 0;
 }
