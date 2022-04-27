e37e0d58b76b56170bf50b195eee3bc3f8b05f58
Tomard Jamis <tjamis@su-b2.msk.21-school.ru>
2022-04-27 20:06:44 +0300

    fix 06/ex00

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -1,122 +1,132 @@
 #include <iostream>
-#include <stdlib.h>
-#include <climits>
-#include <cerrno>
 #include <iomanip>
+#include <limits>
+#include <cerrno>
+#include <cmath>
+#include <stdint.h>
 
-void nan_print()
+void cast_char(char arg)
 {
-	std::cout << "char: impossible"<< std::endl;
-	std::cout << "int: impossible"<< std::endl;
-	std::cout << "double: nan"<< std::endl;
-	std::cout << "float: nanf"<< std::endl;
+	std::cout << "char: '" << arg << "'" << std::endl;
+	std::cout << "int: " << static_cast<int> (arg) << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float> (arg) << "f" << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double> (arg) << std::endl;
 }
 
-void inf_print(int i)
+void cast_int(int arg)
 {
-	std::cout << "char: impossible"<< std::endl;
-	std::cout << "int: impossible"<< std::endl;
-	if (i == 0)
-	{
-		std::cout << "double: +inf"<< std::endl;
-		std::cout << "float: +inff"<< std::endl;
-	}
+	if (arg > 127 || arg < -128)
+		std::cout << "char: impossible\n";
+	else if(isprint(arg))
+		std::cout << "char: '" << static_cast<char> (arg) << "'" << std::endl;
+	else 
+		std::cout << "char: Non displayable\n";
+	std::cout << "int: " << arg << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float> (arg) << "f" << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double> (arg) << std::endl;
+}
+
+void cast_float(float arg)
+{
+	if (arg > 127 || arg < -128)
+		std::cout << "char: impossible\n";
+	else if(isprint(arg))
+		std::cout << "char: '" << static_cast<char> (arg) << "'" << std::endl;
+	else 
+		std::cout << "char: Non displayable\n";
+	if (static_cast<double>(arg) >= INT32_MIN && static_cast<double>(arg) <= INT32_MAX)
+		std::cout << "int: " << static_cast<int> (arg) << std::endl;
 	else
-	{
-		std::cout << "double: -inf"<< std::endl;
-		std::cout << "float: -inff"<< std::endl;
-	}
-}
-
-double to_double(std::string &s)
-{
-	char *end;
-	errno = 0;
-	if (s.size() == 1 && !isdigit(s[0]))
-		return static_cast<double>(s[0]);
-	if(s[s.size() - 1] == 'f')
-		s[s.size() - 1] = 0;
-	double result = std::strtod(s.c_str(), &end);
-	if (errno == ERANGE)
-		throw std::out_of_range("todouble: string is out of range");
-	if (s.length() == 0 || *end != '\0')
-		throw std::invalid_argument("todouble: invalid string");
-	return result;
-}
-
-void cast_char(std::string str)
-{
-	std::cout << "char: ";
-	try
-	{
-		if (isprint(static_cast<int>(to_double(str))))
-			std::cout << static_cast<char>(to_double(str)) << std::endl;
-		else
-			std::cout <<  "Non displayable" << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout <<  "impossible" << std::endl;
-	}
+		std::cout << "int: impossible\n";
+	
+	std::cout << std::fixed << std::setprecision(1) << "float: " << arg << "f" << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "double: " << static_cast<double> (arg) << std::endl;
 	
 }
 
-void cast_int(std::string str)
+void cast_double(double arg)
 {
-	try
-	{
-		std::cout << "int: " << static_cast<int>(to_double(str)) << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout <<  "impossible" << std::endl;
-	}
+	if (arg > 127 || arg < -128)
+		std::cout << "char: impossible\n";
+	else if(isprint(arg))
+		std::cout << "char: '" << static_cast<char> (arg) << "'" << std::endl;
+	else 
+		std::cout << "char: Non displayable\n";
+
+	if (arg >= INT32_MIN && arg <= INT32_MAX)
+		std::cout << "int: " << static_cast<int> (arg) << std::endl;
+	else
+		std::cout << "int: impossible\n";
+	
+	std::cout << std::fixed << std::setprecision(1) << "float: " << static_cast<float> (arg) << "f" << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "double: " << arg << std::endl;
 }
 
-void cast_double(std::string str)
+void print_error()
 {
-	try
-	{
-		std::cout  << std::fixed << std::setprecision(1) << "double: " << to_double(str) << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << "nan" << std::endl;
-	}
+	std::cout << "char: impossible\n";
+	std::cout << "int: impossible\n";
+	std::cout << "float: impossible\n";
+	std::cout << "double: impossible\n";
 }
 
-void cast_float(std::string str)
+void convert(std::string arg)
 {
-	try
+	errno = 0;
+	char *endp = 0;
+	int size = arg.size();
+	if (size == 1 && !isdigit(arg[0]))
 	{
-		std::cout  << std::fixed << std::setprecision(1) << "float: " << to_double(str) << "f" << std::endl;
+		cast_char(arg[0]);
+		return ;
 	}
-	catch(const std::exception& e)
+
+	int i = 1;
+	for (; i < size; i++)
 	{
-		std::cout << "nanf" << std::endl;
+		if (!isdigit(arg[i]))
+			break;
 	}
+
+	if (i == size)
+	{
+		long val_int = strtol(arg.c_str(), &endp, 10);
+		if (errno == ERANGE || *endp != 0 || val_int < INT32_MIN || val_int > INT32_MAX)
+		{
+			print_error();
+			return ;
+		}
+		cast_int(val_int);
+		return ;
+	}
+
+	if (arg[size - 1] == 'f' && arg.compare(std::string("-+").find(arg[0]) != std::string::npos, 4, "inf"))
+	{
+		arg[size - 1] = 0;
+		float val_float = strtof(arg.c_str(), &endp);
+		if (errno == ERANGE || *endp != 0)
+		{
+			print_error();
+			return ;
+		}
+		cast_float(val_float);
+		return ;
+	}
+	
+	double val_double = strtod(arg.c_str(), &endp);
+	if (errno == ERANGE || *endp != 0)
+	{
+		print_error();
+		return ;
+	}
+	cast_double(val_double);
+	return ;
 }
 
 int main(int argc, char **argv)
 {
-	std::string arg = argv[1];
-	int i = std::string("+-").find(arg[0]) != std::string::npos;
-	std::cout << i;
-	if(argc != 2)
+	if (argc != 2)
 		return (1);
-	if(!arg.compare(0, 3, "nan"))
-	{
-		nan_print();
-		return 0;
-	}
-	if(!arg.compare(i, 3, "inf"))
-	{
-		inf_print(arg[0] == '-');
-		return 0;
-	}
-	cast_char(arg);
-	cast_int(arg);
-	cast_double(arg);
-	cast_float(arg);
+	convert(argv[1]);
 	return 0;
 }
